2eac514aa87ce41872e5429b5ae360301ecb9beb
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-11-24 22:57:06 +0700

    fix: error import libraries in user service

--- identity-service/src/main/java/com/haodk/identity/service/UserService.java
+++ identity-service/src/main/java/com/haodk/identity/service/UserService.java
@@ -1,8 +1,5 @@
 package com.haodk.identity.service;
 
-import java.util.HashSet;
-import java.util.List;
-
 import com.haodk.identity.mapper.ProfileMapper;
 import com.haodk.identity.repository.httpclient.ProfileClient;
 import org.springframework.security.access.prepost.PostAuthorize;
@@ -27,6 +24,9 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashSet;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
